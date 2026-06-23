5065aecd7ae5e5a4b68f9db17bbca472bb2e93cc
jjangjun0 <jangjun012@gmail.com>
2026-06-23 12:07:58 +0900

    몫 구하기

---
+++ src/main/java/org/example/Test003.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Test003 {
+    public int solution(int num1, int num2) {
+        return num1 / num2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
