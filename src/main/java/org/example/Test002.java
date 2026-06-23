17b52d5efb460f103add61ac5d82f113e11cbc63
jjangjun0 <jangjun012@gmail.com>
2026-06-23 12:06:07 +0900

    두 수의 곱 구하기

---
+++ src/main/java/org/example/Test002.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Test002 {
+    public static int solution(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(100, 2));
+    }
+}
