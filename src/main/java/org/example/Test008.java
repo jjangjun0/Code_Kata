26f4b0acfb99196ec8d96d17fc2c7796fe8d502a
jjangjun0 <jangjun012@gmail.com>
2026-06-24 10:40:18 +0900

    각도기

---
+++ src/main/java/org/example/Test008.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Test008 {
+    public static int solution(int angle) {
+        int answer = 0;
+        if (angle > 0 && angle < 90)
+            answer = 1;
+        else if (angle == 90)
+            answer = 2;
+        else if (angle > 90 && angle < 180)
+            answer = 3;
+        else if (angle == 180)
+            answer = 4;
+        return answer;
+    }
+    public static void main(String[] args) {
+        System.out.println(solution(90));
+    }
+}
