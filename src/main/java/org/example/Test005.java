51357ffb851cd9424f82359b4f57b9f3eb17d13f
jjangjun0 <jangjun012@gmail.com>
2026-06-24 10:27:18 +0900

    숫자 비교하기

---
+++ src/main/java/org/example/Test005.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Test005 {
+    public static int solution(int num1, int num2) {
+        return (num1 == num2 ? 1 : -1);
+    }
+    public static void main(String[] args) {
+        System.out.println(solution(1, 2));
+    }
+}
