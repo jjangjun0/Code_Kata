356a35b753ec45ee496e165887ea45f2522feab5
jjangjun0 <jangjun012@gmail.com>
2026-06-24 10:24:44 +0900

    나이 출력

---
+++ src/main/java/org/example/Test004.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Test004 {
+    public static int solution(int age) {
+        return 2022 - age + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(20));
+    }
+}
