2fae4fe6b6a04042d2c0ed73f0d7fe5abf88bd44
jjangjun0 <jangjun012@gmail.com>
2026-06-24 10:36:01 +0900

    두 수의 나눗셈

---
+++ src/main/java/org/example/Test007.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Test007 {
+    public static int solution(int num1, int num2) {
+        return num1 * 1000 / num2;
+    }
+    public static void main(String[] args) {
+        System.out.println(solution(7, 3));
+    }
+}
