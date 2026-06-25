9fe48788861e985d5bcf86a6d06752b35c9327a4
jjangjun0 <jangjun012@gmail.com>
2026-06-25 10:52:51 +0900

    나머지가 1이 되는 수 찾기

---
+++ src/main/java/org/example/Test015.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Test015 {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 1) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
